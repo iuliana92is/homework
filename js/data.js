d85db3304e16ecf754c69367177eaa1fa2527305
unknown <iuciobanu@EN61033.endava.net>
2015-06-26 16:17:54 +0300

    modificari js ajutata de vali

---
+++ js/data.js
@@ -0,0 +1,100 @@
+var data = [{
+    fullName: "Ciba Mumu",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: "UI Developer",
+    grade: "Senior Engineer",
+    allocationStatus: "Allocated",
+    project: "Trinity Miror",
+    date: "10/05/2014"
+}, {
+    fullName: "Vranceanu Valentin",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: "Java Developer",
+    grade: "Senior Engineer",
+    allocationStatus: "Not Allocated",
+    project: "Visa",
+    date: "10/05/2014"
+}, {
+    fullName: "Bogdan Volosincu",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: ".Net Developer",
+    grade: "Senior Technician",
+    allocationStatus: "Allocated",
+    project: "Graduate",
+    date: "10/05/2014"
+}, {
+    fullName: "CarmenBudau",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: "Bussines Analyst",
+    grade: "Engineer",
+    allocationStatus: "Not Allocated",
+    project: "Denplan",
+    date: "10/05/2014"
+}, {
+    fullName: "Iuliana Ciobanu",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: "UI Developer",
+    grade: "seniorEngineer",
+    allocationStatus: "Allocated",
+    project: "Trinity Miror",
+    date: "10/05/2014"
+}, {
+    fullName: "Iulia Cojocaru",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: "Java Developer",
+    grade: "Senior Engineer",
+    allocationStatus: "Not Allocated",
+    project: "Visa",
+    date: "10/05/2014"
+}, {
+    fullName: " Ionut Popescu",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: ".Net Developer",
+    grade: "Senior Technician",
+    allocationStatus: "Allocated",
+    project: "Graduate",
+    date: "10/05/2014"
+}, {
+    fullName: "Alexandru Bolboceanu",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 0 3/ 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: "Bussines Analyst",
+    grade: "Engineer",
+    allocationStatus: "Not Allocated",
+    project: "Denplan",
+    date: "10/05/2014"
+}, {
+	fullName: "Marius Ionescu",
+    deliveryUnit: "ISD_Iasi",
+    dateOfStart: "12 / 03 / 2015",
+    lineManager: "Bogdan Volosincu",
+    projectManager: "Andrei Ursache",
+    jobTitle: "Bussines Bussines Analyst Analyst Analyst",
+    grade: "Engineer",
+    allocationStatus: "Not Allocated",
+    project: "Denplan",
+    date: "10/05/2014"
+}];
